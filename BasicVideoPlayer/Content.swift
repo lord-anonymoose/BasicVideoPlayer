185568988a3bbe31112bdce2102d1b6acbb2cb3e
Philipp Lazarev <philipp.lazareff@gmail.com>
2024-06-15 12:27:32 +0300

    Working on Project structure

---
+++ BasicVideoPlayer/Content.swift
@@ -0,0 +1,11 @@
+//
+//  Content.swift
+//  BasicVideoPlayer
+//
+//  Created by Philipp Lazarev on 15.06.2024.
+//
+
+import Foundation
+import UIKit
+
+let logoImage = UIImage(systemName: "Stringplay.rectangle.fill")?.withTintColor(.systemRed)
